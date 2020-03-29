0a9f50f822651566605ab8154c5d849349734ab4
Thomas <thomas@craftacademy.se>
2020-03-29 23:05:01 +0200

    fixes failing assssertions in test and formats code

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -1,48 +1,50 @@
 import React from 'react'
 import {
-	CardNumberElement,
-	CardExpiryElement,
-	CardCVCElement,
-	injectStripe
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  injectStripe
 } from "react-stripe-elements";
 import axios from 'axios'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const SubscriptionForm = props => {
-	const dispatch = useDispatch()
-	const submitPayment = async (event) => {
-		event.preventDefault()
-		let stripeResponse = await props.stripe.createToken()
-		let token = stripeResponse.token.id
-		let paymentStatus = await axios.post('http://localhost:3000/api/subscriptions', { stripeToken: token })
-		if (paymentStatus.data.status === 'paid') {
-			dispatch({
-				type: 'FLASH_MESSAGE',
-				payload: {
-					flashMessage: "Thank you for your business!",
-					showSubscriptionForm: false,
-					currentUser: { role: 'subscriber' }
-				}
-			})
-			dispatch({ type: 'HIDE_ARTICLE' })
+  const dispatch = useDispatch()
+  const { email } = useSelector(state => state.currentUser)
+  const submitPayment = async (event) => {
+    event.preventDefault()
+    let stripeResponse = await props.stripe.createToken()
+    let token = stripeResponse.token.id
+    let paymentStatus = await axios.post('http://localhost:3000/api/subscriptions', { stripeToken: token, email: email })
+    if (paymentStatus.data.status === 'paid') {
+      // 7th request??
+      dispatch({
+        type: 'FLASH_MESSAGE',
+        payload: {
+          flashMessage: "Thank you for your business!",
+          showSubscriptionForm: false,
+          currentUser: { role: 'subscriber' }
+        }
+      })
+      dispatch({ type: 'HIDE_ARTICLE' })
 
-		}
-	}
-	return (
-		<form id="subscription-form">
-			<h1>FORM!</h1>
-			<label>CC Number</label>
-			<CardNumberElement />
-			<label>Expiry Date</label>
-			<CardExpiryElement />
-			<label>CVC</label>
-			<CardCVCElement />
-			<button
-				onClick={(event) => submitPayment(event)}
-			>Submit payment</button>
-		</form>
+    }
+  }
+  return (
+    <form id="subscription-form">
+      <h1>FORM!</h1>
+      <label>CC Number</label>
+      <CardNumberElement />
+      <label>Expiry Date</label>
+      <CardExpiryElement />
+      <label>CVC</label>
+      <CardCVCElement />
+      <button
+        onClick={(event) => submitPayment(event)}
+      >Submit payment</button>
+    </form>
 
-	)
+  )
 }
 
 export default injectStripe(SubscriptionForm)
